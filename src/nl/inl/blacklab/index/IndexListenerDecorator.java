8ac7d9bbd2d5469752b0cbaf31a088d80df792bb
niestadt <jan.niestadt@inl.nl>
2015-01-20 15:26:44 +0100

    Added IndexListenerDecorator.
    May be used to extend the functionality of an existing listener
    instance.

---
+++ src/nl/inl/blacklab/index/IndexListenerDecorator.java
@@ -0,0 +1,142 @@
+package nl.inl.blacklab.index;
+
+import java.io.File;
+
+/**
+ * Decorate an IndexListener.
+ */
+public abstract class IndexListenerDecorator extends IndexListener {
+
+	IndexListener subject;
+
+	public IndexListenerDecorator(IndexListener subject) {
+		this.subject = subject;
+	}
+
+	@Override
+	public synchronized void fileStarted(String name) {
+		subject.fileStarted(name);
+	}
+
+	@Override
+	public synchronized void fileDone(String name) {
+		subject.fileDone(name);
+	}
+
+	@Override
+	public synchronized void charsDone(long charsDone) {
+		subject.charsDone(charsDone);
+	}
+
+	@Override
+	public synchronized void documentStarted(String name) {
+		subject.documentStarted(name);
+	}
+
+	@Override
+	public synchronized void documentDone(String name) {
+		subject.documentDone(name);
+	}
+
+	@Override
+	public synchronized long getFilesProcessed() {
+		return subject.getFilesProcessed();
+	}
+
+	@Override
+	public synchronized long getCharsProcessed() {
+		return subject.getCharsProcessed();
+	}
+
+	@Override
+	public synchronized long getTokensProcessed() {
+		return subject.getTokensProcessed();
+	}
+
+	@Override
+	public synchronized long getDocsDone() {
+		return subject.getDocsDone();
+	}
+
+	@Override
+	public void indexStart() {
+		subject.indexStart();
+	}
+
+	@Override
+	public void indexEnd() {
+		subject.indexEnd();
+	}
+
+	@Override
+	public void closeStart() {
+		subject.closeStart();
+	}
+
+	@Override
+	public void closeEnd() {
+		subject.closeEnd();
+	}
+
+	@Override
+	public long getIndexTime() {
+		return subject.getIndexTime();
+	}
+
+	@Override
+	public long getOptimizeTime() {
+		return subject.getOptimizeTime();
+	}
+
+	@Override
+	public long getCloseTime() {
+		return subject.getCloseTime();
+	}
+
+	@Override
+	public void indexerCreated(Indexer indexer) {
+		subject.indexerCreated(indexer);
+	}
+
+	@Override
+	public void indexerClosed() {
+		subject.indexerClosed();
+	}
+
+	@Override
+	public long getTotalTime() {
+		return subject.getTotalTime();
+	}
+
+	@Override
+	public void luceneDocumentAdded() {
+		subject.luceneDocumentAdded();
+	}
+
+	@Override
+	public void tokensDone(int n) {
+		subject.tokensDone(n);
+	}
+
+	@Override
+	public boolean errorOccurred(String error, String unitType, File unit,
+			File subunit) {
+		return subject.errorOccurred(error, unitType, unit, subunit);
+	}
+
+	@Override
+	public int getErrors() {
+		return subject.getErrors();
+	}
+
+	@Override
+	public void rollbackStart() {
+		subject.rollbackStart();
+	}
+
+	@Override
+	public void rollbackEnd() {
+		subject.rollbackEnd();
+	}
+
+}
