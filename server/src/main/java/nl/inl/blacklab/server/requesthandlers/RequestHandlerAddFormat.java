4770fd49669412907a9eedfa7dcf10730996fe0d
Jan Niestadt <jan.niestadt@inl.nl>
2017-08-10 17:02:16 +0200

    Made sure error in format configuration is reported cleanly.

--- server/src/main/java/nl/inl/blacklab/server/requesthandlers/RequestHandlerAddFormat.java
+++ server/src/main/java/nl/inl/blacklab/server/requesthandlers/RequestHandlerAddFormat.java
@@ -3,6 +3,7 @@ package nl.inl.blacklab.server.requesthandlers;
 import java.io.File;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.fileupload.FileItem;
 
@@ -28,26 +29,30 @@ public class RequestHandlerAddFormat extends RequestHandler {
 	}
 
 	@Override
-	public int handle(DataStream ds) throws BlsException {
+	public int handle(final DataStream ds) throws BlsException {
 		debug(logger, "REQ add format: " + indexName);
 
-		FileUploadHandler.handleRequest(ds, request, "data", new UploadedFileTask() {
-			@Override
-			public void handle(FileItem fi) throws Exception {
-				// Get the uploaded file parameters
-				String fileName = fi.getName();
-				if (!fileName.matches("[\\w_\\-]+(\\.blf)?\\.(ya?ml|json)"))
-				    throw new BadRequest("ILLEGAL_INDEX_NAME", "Format configuration name may only contain letters, digits, underscore and dash, and must end with .yaml or .json (or .blf.yaml/.blf.json)");
-				String formatIdentifier = ConfigInputFormat.stripExtensions(fileName);
-				boolean isJson = fileName.endsWith(".json");
-                File userFormatDir = indexMan.getUserFormatDir(user.getUserId());
-				File formatFile = new File(userFormatDir, formatIdentifier + ".blf." + (isJson ? "json" : "yaml"));
-				fi.write(formatFile);
-                ConfigInputFormat f = new ConfigInputFormat(formatFile);
-                f.setName(user.getUserId() + ":" + f.getName()); // prefix with user id to avoid collisions
-                DocumentFormats.register(f);
-			}
-		});
+        try {
+            FileUploadHandler.handleRequest(ds, request, "data", new UploadedFileTask() {
+    			@Override
+    			public void handle(FileItem fi) throws Exception {
+    				// Get the uploaded file parameters
+    				String fileName = fi.getName();
+    				if (!fileName.matches("[\\w_\\-]+(\\.blf)?\\.(ya?ml|json)"))
+    				    throw new BadRequest("ILLEGAL_INDEX_NAME", "Format configuration name may only contain letters, digits, underscore and dash, and must end with .yaml or .json (or .blf.yaml/.blf.json)");
+    				String formatIdentifier = ConfigInputFormat.stripExtensions(fileName);
+    				boolean isJson = fileName.endsWith(".json");
+                    File userFormatDir = indexMan.getUserFormatDir(user.getUserId());
+    				File formatFile = new File(userFormatDir, formatIdentifier + ".blf." + (isJson ? "json" : "yaml"));
+    				fi.write(formatFile);
+                    ConfigInputFormat f = new ConfigInputFormat(formatFile);
+                    f.setName(user.getUserId() + ":" + f.getName()); // prefix with user id to avoid collisions
+                    DocumentFormats.register(f);
+    			}
+            });
+        } catch (IllegalArgumentException e) {
+            return Response.error(ds, "CONFIG_ERROR", "Error in format configuration: " + e.getMessage(), HttpServletResponse.SC_BAD_REQUEST);
+        }
 		return Response.success(ds, "Format added.");
 	}
 
