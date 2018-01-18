21d9bab2e6742c202984f0ba783c16cc62d43391
Jan Niestadt <jan.niestadt@ivdnt.org>
2018-01-18 14:35:34 +0100

    Show indexing progress on index structure page as well.

--- server/src/main/java/nl/inl/blacklab/server/requesthandlers/RequestHandlerIndexStructure.java
+++ server/src/main/java/nl/inl/blacklab/server/requesthandlers/RequestHandlerIndexStructure.java
@@ -6,6 +6,7 @@ import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 
+import nl.inl.blacklab.index.IndexListener;
 import nl.inl.blacklab.search.Searcher;
 import nl.inl.blacklab.search.indexstructure.ComplexFieldDesc;
 import nl.inl.blacklab.search.indexstructure.IndexStructure;
@@ -14,6 +15,8 @@ import nl.inl.blacklab.search.indexstructure.MetadataFieldDesc;
 import nl.inl.blacklab.server.BlackLabServer;
 import nl.inl.blacklab.server.datastream.DataStream;
 import nl.inl.blacklab.server.exceptions.BlsException;
+import nl.inl.blacklab.server.index.Index;
+import nl.inl.blacklab.server.index.Index.IndexStatus;
 import nl.inl.blacklab.server.jobs.User;
 import nl.inl.util.StringUtil;
 
@@ -33,98 +36,114 @@ public class RequestHandlerIndexStructure extends RequestHandler {
 
 	@Override
 	public int handle(DataStream ds) throws BlsException {
-		Searcher searcher = getSearcher();
-		IndexStructure struct = searcher.getIndexStructure();
+        Index index = indexMan.getIndex(indexName);
+        synchronized (index) {
+    		Searcher searcher = index.getSearcher();
+    		IndexStructure struct = searcher.getIndexStructure();
 
-		// Assemble response
-		ds.startMap()
-			.entry("indexName", indexName)
-			.entry("displayName", struct.getDisplayName())
-			.entry("description", struct.getDescription())
-			.entry("status", indexMan.getIndex(indexName).getStatus())
-			.entry("contentViewable", struct.contentViewable());
-		String documentFormat = struct.getDocumentFormat();
-		if (documentFormat != null && documentFormat.length() > 0)
-			ds.entry("documentFormat", documentFormat);
-		if (struct.getTokenCount() > 0)
-			ds.entry("tokenCount", struct.getTokenCount());
+    		// Assemble response
+    		IndexStatus status = indexMan.getIndex(indexName).getStatus();
+            ds.startMap()
+    			.entry("indexName", indexName)
+    			.entry("displayName", struct.getDisplayName())
+    			.entry("description", struct.getDescription())
+    			.entry("status", status)
+    			.entry("contentViewable", struct.contentViewable());
 
-		ds.startEntry("versionInfo").startMap()
-			.entry("blackLabBuildTime", struct.getIndexBlackLabBuildTime())
-			.entry("blackLabVersion", struct.getIndexBlackLabVersion())
-			.entry("indexFormat", struct.getIndexFormat())
-			.entry("timeCreated", struct.getTimeCreated())
-			.entry("timeModified", struct.getTimeModified())
-		.endMap().endEntry();
-
-		ds.startEntry("fieldInfo").startMap()
-			.entry("pidField", StringUtil.nullToEmpty(struct.pidField()))
-			.entry("titleField", StringUtil.nullToEmpty(struct.titleField()))
-			.entry("authorField", StringUtil.nullToEmpty(struct.authorField()))
-			.entry("dateField", StringUtil.nullToEmpty(struct.dateField()))
-		.endMap().endEntry();
-
-		ds.startEntry("complexFields").startMap();
-		// Complex fields
-		//DataObjectMapAttribute doComplexFields = new DataObjectMapAttribute("complexField", "name");
-		for (String name: struct.getComplexFields()) {
-			ds.startAttrEntry("complexField", "name", name);
-
-	        Set<String> setShowValuesFor = searchParam.listValuesFor();
-	        Set<String> setShowSubpropsFor = searchParam.listSubpropsFor();
-			ComplexFieldDesc fieldDesc = struct.getComplexFieldDesc(name);
-            RequestHandlerFieldInfo.describeComplexField(ds, null, name, fieldDesc, searcher, setShowValuesFor, setShowSubpropsFor);
-
-			ds.endAttrEntry();
-		}
-		ds.endMap().endEntry();
-
-		ds.startEntry("metadataFields").startMap();
-		// Metadata fields
-		//DataObjectMapAttribute doMetaFields = new DataObjectMapAttribute("metadataField", "name");
-		for (String name: struct.getMetadataFields()) {
-			ds.startAttrEntry("metadataField", "name", name);
-
-            MetadataFieldDesc fd = struct.getMetadataFieldDesc(name);
-			RequestHandlerFieldInfo.describeMetadataField(ds, null, name, fd, true);
-
-			ds.endAttrEntry();
-		}
-		ds.endMap().endEntry();
-
-        Map<String, MetadataGroup> metaGroups = struct.getMetaFieldGroups();
-		Set<String> metadataFieldsNotInGroups = new HashSet<>(struct.getMetadataFields());
-        for (MetadataGroup metaGroup: metaGroups.values()) {
-            for (String field: metaGroup.getFields()) {
-                metadataFieldsNotInGroups.remove(field);
-            }
-        }
-        ds.startEntry("metadataFieldGroups").startList();
-        boolean addedRemaining = false;
-        for (MetadataGroup metaGroup: metaGroups.values()) {
-            ds.startItem("metadataFieldGroup").startMap();
-            ds.entry("name", metaGroup.getName());
-            ds.startEntry("fields").startList();
-            for (String field: metaGroup.getFields()) {
-                ds.item("field", field);
-            }
-            if (!addedRemaining && metaGroup.addRemainingFields()) {
-                addedRemaining = true;
-                for (String field: metadataFieldsNotInGroups) {
-                    ds.item("field", field);
+            if (status.equals(IndexStatus.INDEXING)) {
+                IndexListener indexProgress = index.getIndexerListener();
+                synchronized (indexProgress) {
+                    ds.startEntry("indexProgress").startMap()
+                    .entry("filesProcessed", indexProgress.getFilesProcessed())
+                    .entry("docsDone", indexProgress.getDocsDone())
+                    .entry("tokensProcessed", indexProgress.getTokensProcessed())
+                    .endMap().endEntry();
                 }
             }
+
+    		String documentFormat = struct.getDocumentFormat();
+    		if (documentFormat != null && documentFormat.length() > 0)
+    			ds.entry("documentFormat", documentFormat);
+    		if (struct.getTokenCount() > 0)
+    			ds.entry("tokenCount", struct.getTokenCount());
+
+    		ds.startEntry("versionInfo").startMap()
+    			.entry("blackLabBuildTime", struct.getIndexBlackLabBuildTime())
+    			.entry("blackLabVersion", struct.getIndexBlackLabVersion())
+    			.entry("indexFormat", struct.getIndexFormat())
+    			.entry("timeCreated", struct.getTimeCreated())
+    			.entry("timeModified", struct.getTimeModified())
+    		.endMap().endEntry();
+
+    		ds.startEntry("fieldInfo").startMap()
+    			.entry("pidField", StringUtil.nullToEmpty(struct.pidField()))
+    			.entry("titleField", StringUtil.nullToEmpty(struct.titleField()))
+    			.entry("authorField", StringUtil.nullToEmpty(struct.authorField()))
+    			.entry("dateField", StringUtil.nullToEmpty(struct.dateField()))
+    		.endMap().endEntry();
+
+    		ds.startEntry("complexFields").startMap();
+    		// Complex fields
+    		//DataObjectMapAttribute doComplexFields = new DataObjectMapAttribute("complexField", "name");
+    		for (String name: struct.getComplexFields()) {
+    			ds.startAttrEntry("complexField", "name", name);
+
+    	        Set<String> setShowValuesFor = searchParam.listValuesFor();
+    	        Set<String> setShowSubpropsFor = searchParam.listSubpropsFor();
+    			ComplexFieldDesc fieldDesc = struct.getComplexFieldDesc(name);
+                RequestHandlerFieldInfo.describeComplexField(ds, null, name, fieldDesc, searcher, setShowValuesFor, setShowSubpropsFor);
+
+    			ds.endAttrEntry();
+    		}
+    		ds.endMap().endEntry();
+
+    		ds.startEntry("metadataFields").startMap();
+    		// Metadata fields
+    		//DataObjectMapAttribute doMetaFields = new DataObjectMapAttribute("metadataField", "name");
+    		for (String name: struct.getMetadataFields()) {
+    			ds.startAttrEntry("metadataField", "name", name);
+
+                MetadataFieldDesc fd = struct.getMetadataFieldDesc(name);
+    			RequestHandlerFieldInfo.describeMetadataField(ds, null, name, fd, true);
+
+    			ds.endAttrEntry();
+    		}
+    		ds.endMap().endEntry();
+
+            Map<String, MetadataGroup> metaGroups = struct.getMetaFieldGroups();
+    		Set<String> metadataFieldsNotInGroups = new HashSet<>(struct.getMetadataFields());
+            for (MetadataGroup metaGroup: metaGroups.values()) {
+                for (String field: metaGroup.getFields()) {
+                    metadataFieldsNotInGroups.remove(field);
+                }
+            }
+            ds.startEntry("metadataFieldGroups").startList();
+            boolean addedRemaining = false;
+            for (MetadataGroup metaGroup: metaGroups.values()) {
+                ds.startItem("metadataFieldGroup").startMap();
+                ds.entry("name", metaGroup.getName());
+                ds.startEntry("fields").startList();
+                for (String field: metaGroup.getFields()) {
+                    ds.item("field", field);
+                }
+                if (!addedRemaining && metaGroup.addRemainingFields()) {
+                    addedRemaining = true;
+                    for (String field: metadataFieldsNotInGroups) {
+                        ds.item("field", field);
+                    }
+                }
+                ds.endList().endEntry();
+                ds.endMap().endItem();
+            }
             ds.endList().endEntry();
-            ds.endMap().endItem();
+
+    		// Remove any empty settings
+    		//response.removeEmptyMapValues();
+
+    		ds.endMap();
+
+    		return HTTP_OK;
         }
-        ds.endList().endEntry();
-
-		// Remove any empty settings
-		//response.removeEmptyMapValues();
-
-		ds.endMap();
-
-		return HTTP_OK;
 	}
 
 }
