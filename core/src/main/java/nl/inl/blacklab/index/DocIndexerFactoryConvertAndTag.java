9ed4aae027014a2b34bf9b7c536d5e754c16e98f
Koen Mertens <koen.mertens@ivdnt.org>
2018-04-05 11:30:15 +0200

    Improve displayName of plugin-based Formats

--- core/src/main/java/nl/inl/blacklab/index/DocIndexerFactoryConvertAndTag.java
+++ core/src/main/java/nl/inl/blacklab/index/DocIndexerFactoryConvertAndTag.java
@@ -87,7 +87,7 @@ public class DocIndexerFactoryConvertAndTag implements DocIndexerFactory {
 
 		// generate displayName
 		private static String displayName(ConvertPlugin converter, TagPlugin tagger, Format outputFormat) {
-			return tagger.getInputFormat() + "<" + tagger.getDisplayName() + "<" + converter.getDisplayName();
+			return"Generated " + StringUtils.capitalize(converter.getOutputFormat()) + " from " + converter.getDisplayName() + " and " + tagger.getDisplayName();
 		}
 
 		// generate description
@@ -127,7 +127,7 @@ public class DocIndexerFactoryConvertAndTag implements DocIndexerFactory {
 	 */
 	public static void initPlugins(ObjectNode pluginConfig) {
 		if (isInitialized)
-			throw new IllegalStateException("PluginManager already initialized"); 
+			throw new IllegalStateException("PluginManager already initialized");
 
 		logger.debug("Initializing plugin system");
 
@@ -173,18 +173,20 @@ public class DocIndexerFactoryConvertAndTag implements DocIndexerFactory {
 				T plugin = it.next();
 				id = plugin.getId();
 				if (!PLUGINID_PATTERN.matcher(id).matches()) {
-					logger.info("Plugin id " + id + " (class " + plugin.getClass().getCanonicalName() + ") is not a valid id; ignoring plugin.");
+					logger.warn("Plugin id " + id + " (class " + plugin.getClass().getCanonicalName() + ") is not a valid id; ignoring plugin.");
 					continue;
 				}
 
 				JsonNode config = pluginConfig.get(plugin.getId());
+
+				logger.info("Initializing plugin " + plugin.getDisplayName());
 				if (config == null || config instanceof NullNode)
 					plugin.init(null);
 				else
 					plugin.init(YamlJsonReader.obj(config, plugin.getId()));
 
 				plugins.put(id, plugin);
-				logger.debug("Initialized plugin " + plugin.getDisplayName());
+				logger.info("Initialized plugin " + plugin.getDisplayName());
 			} catch (ServiceConfigurationError e) {
 				logger.error("Plugin failed to load: " + e.getMessage(), e);
 			} catch (PluginException e) {
