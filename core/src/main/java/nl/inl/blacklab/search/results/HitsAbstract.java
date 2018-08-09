95fe6926e5ad047af5c4e163548b380c3712fd5d
Jan Niestadt <jan.niestadt@inl.nl>
2018-08-09 15:14:36 +0200

    Pulled stats() up to HitsAbstract.

--- core/src/main/java/nl/inl/blacklab/search/results/HitsAbstract.java
+++ core/src/main/java/nl/inl/blacklab/search/results/HitsAbstract.java
@@ -40,14 +40,186 @@ public abstract class HitsAbstract implements Iterable<Hit>, Prioritizable {
 
     public abstract int numberOfDocs();
 
-    public abstract int totalSize();
-
     public abstract int size();
+
+    public abstract int totalSize();
 
     public abstract boolean sizeAtLeast(int lowerBound);
 
-    public abstract ResultsStatsHitsDocs stats();
+    // Stats about hits fetching
+    // --------------------------------------------------------------------------
+    
+    private ResultsNumber resultsNumberHitsProcessed = new ResultsNumber() {
+        @Override
+        public int total() {
+            return size();
+        }
 
+        @Override
+        public boolean atLeast(int lowerBound) {
+            return sizeAtLeast(lowerBound);
+        }
+
+        @Override
+        public int soFar() {
+            return countSoFarHitsRetrieved();
+        }
+
+        @Override
+        public boolean done() {
+            return doneFetchingHits();
+        }
+
+        @Override
+        public boolean exceededMaximum() {
+            return maxHitsRetrieved();
+        }
+
+        @Override
+        public int maximum() {
+            return settings.maxHitsToRetrieve();
+        }
+    };
+    
+    ResultsNumber resultsNumberHitsCounted = new ResultsNumber() {
+        @Override
+        public int total() {
+            return totalSize();
+        }
+
+        @Override
+        public boolean atLeast(int lowerBound) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public int soFar() {
+            return countSoFarHitsCounted();
+        }
+
+        @Override
+        public boolean done() {
+            return doneFetchingHits();
+        }
+
+        @Override
+        public boolean exceededMaximum() {
+            return maxHitsCounted();
+        }
+
+        @Override
+        public int maximum() {
+            return settings.maxHitsToCount();
+        }
+    };
+    
+    private ResultsNumber resultsNumberDocsProcessed = new ResultsNumber() {
+        @Override
+        public int total() {
+            return numberOfDocs();
+        }
+
+        @Override
+        public boolean atLeast(int lowerBound) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public int soFar() {
+            return countSoFarDocsRetrieved();
+        }
+
+        @Override
+        public boolean done() {
+            return doneFetchingHits();
+        }
+
+        @Override
+        public boolean exceededMaximum() {
+            return maxHitsRetrieved();
+        }
+
+        @Override
+        public int maximum() {
+            throw new UnsupportedOperationException();
+        }
+    };
+    
+    private ResultsNumber resultsNumberDocsCounted = new ResultsNumber() {
+        @Override
+        public int total() {
+            if (done())
+                return soFar();
+            throw new UnsupportedOperationException("Cannot return total docs until all hits have been fetched");
+        }
+
+        @Override
+        public boolean atLeast(int lowerBound) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public int soFar() {
+            return countSoFarDocsCounted();
+        }
+
+        @Override
+        public boolean done() {
+            return doneFetchingHits();
+        }
+
+        @Override
+        public boolean exceededMaximum() {
+            return maxHitsCounted();
+        }
+
+        @Override
+        public int maximum() {
+            throw new UnsupportedOperationException();
+        }
+    };
+    
+    private ResultsStats hitStats = new ResultsStats() {
+        @Override
+        public ResultsNumber processed() {
+            return resultsNumberHitsProcessed;
+        }
+
+        @Override
+        public ResultsNumber counted() {
+            return resultsNumberHitsCounted;
+        }
+    };
+    
+    private ResultsStats docStats = new ResultsStats() {
+        @Override
+        public ResultsNumber processed() {
+            return resultsNumberDocsProcessed;
+        }
+
+        @Override
+        public ResultsNumber counted() {
+            return resultsNumberDocsCounted;
+        }
+    };
+    
+    private ResultsStatsHitsDocs hitsDocsStats = new ResultsStatsHitsDocs() {
+        @Override
+        public ResultsStats hits() {
+            return hitStats;
+        }
+
+        @Override
+        public ResultsStats docs() {
+            return docStats;
+        }
+        
+    };
+    
+    public ResultsStatsHitsDocs stats() {
+        return hitsDocsStats;
+    }
+    
     protected abstract void ensureHitsRead(int number) throws InterruptedException;
 
     protected abstract void ensureAllHitsRead() throws InterruptedException;
