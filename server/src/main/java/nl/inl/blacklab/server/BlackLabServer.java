7858293234a4109e1ad01a523eb56f0cf9b7eba3
James Baird <jamesb@allenai.org>
2019-02-12 13:42:38 -0800

    Surface insufficient memory exceptions

--- server/src/main/java/nl/inl/blacklab/server/BlackLabServer.java
+++ server/src/main/java/nl/inl/blacklab/server/BlackLabServer.java
@@ -22,6 +22,7 @@ import org.apache.logging.log4j.Logger;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 
+import nl.inl.blacklab.exceptions.InsufficientMemoryAvailable;
 import nl.inl.blacklab.exceptions.InterruptedSearch;
 import nl.inl.blacklab.search.BlackLab;
 import nl.inl.blacklab.server.config.BLSConfig;
@@ -276,6 +277,8 @@ public class BlackLabServer extends HttpServlet {
                 httpCode = Response.error(es, e.getBlsErrorCode(), e.getMessage(), e.getHttpStatusCode());
             } catch (InterruptedSearch e) {
                 httpCode = Response.error(es, "INTERRUPTED", "Search was interrupted", HttpServletResponse.SC_SERVICE_UNAVAILABLE);
+            } catch (InsufficientMemoryAvailable e) {
+                httpCode = Response.error(es, "INSUFFICIENT_MEMORY", e.getMessage(), HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
             } catch (RuntimeException e) {
                 httpCode = Response.internalError(es, e, debugMode, "INTERR_HANDLING_REQUEST");
             } finally {
