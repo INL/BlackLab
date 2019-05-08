60e06eacbb819bde36bfe159a34b21a78574965a
Koen Mertens <koen.mertens@ivdnt.org>
2019-05-08 14:39:57 +0200

    Clarify error message in xsl generated from input format

--- server/src/main/java/nl/inl/blacklab/server/requesthandlers/RequestHandlerListInputFormats.java
+++ server/src/main/java/nl/inl/blacklab/server/requesthandlers/RequestHandlerListInputFormats.java
@@ -373,7 +373,9 @@ public class RequestHandlerListInputFormats extends RequestHandler {
                     +
                     "so the words can't be found. " +
                     "To fix this, you will need to add the namespaces in your format and use them in the xpaths. " +
-                    "If your document contains a default namespace, you can declare it as an empty name (\"\": \"http://my-default-namespace.site/namespace\")";
+                    "If your document contains a default namespace, you can declare it as an empty name (\"\": \"http://my-default-namespace.site/namespace\"). "
+                    +
+                    "This may also happen if the generated xslt is applied to a partial document, such as when using the wordstart and wordend parameters when requesting its contents from blacklab-server.";
 
             // Entry point (after optional namespace removal)
             xslt.append(
