796e8a97cbb81bd93f51aed6fa055cb7ba9633f2
Koen Mertens <koen.mertens@ivdnt.org>
2019-06-27 09:51:10 +0200

    More helpful error when query contains unknown annotation

--- core/src/main/java/nl/inl/blacklab/search/textpattern/TextPatternAnnotation.java
+++ core/src/main/java/nl/inl/blacklab/search/textpattern/TextPatternAnnotation.java
@@ -43,13 +43,13 @@ public class TextPatternAnnotation extends TextPattern {
     public BLSpanQuery translate(QueryExecutionContext context) throws InvalidQuery {
         String[] parts = annotationName.split("/", -1);
         if (parts.length > 2)
-            throw new IllegalArgumentException("Annotation name contains more than one colon: " + annotationName);
+            throw new InvalidQuery("Annotation name contains more than one colon: " + annotationName);
         Annotation annotation;
         if (context.index().metadata().subannotationsStoredWithParent()) {
             // Old-style index, where subannotations are stored in their parent's Lucene field
             annotation = context.field().annotation(parts[0]);
             if (annotation == null)
-                throw new IllegalArgumentException("Annotation doesn't exist: " + annotationName);
+                throw new InvalidQuery("Annotation doesn't exist: " + annotationName);
             if (parts.length > 1)
                 annotation = annotation.subannotation(parts[1]);
         } else {
@@ -59,7 +59,7 @@ public class TextPatternAnnotation extends TextPattern {
                 name += AnnotatedFieldNameUtil.SUBANNOTATION_FIELD_PREFIX_SEPARATOR + parts[1];
             annotation = context.field().annotation(name);
             if (annotation == null)
-                throw new IllegalArgumentException("Annotation doesn't exist: " + name);
+                throw new InvalidQuery("Annotation doesn't exist: " + name);
         }
         return input.translate(context.withAnnotation(annotation));
     }
