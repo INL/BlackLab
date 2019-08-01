9ede59ee02d54a145b0682313b11d351104ae01b
Jan Niestadt <jan.niestadt@inl.nl>
2019-08-01 10:26:26 +0200

    Added annotation option allowDuplicateValues (defaults to true).
    Only used for annotations where multipleValues is set to true. If
    allowDuplicateValues is set to false, any duplicate values matched will
    be discarded.
    
    This is useful for example if you want to create and annotation that
    includes both word and lemma, which can be used for simple search.

--- core/src/main/java/nl/inl/blacklab/indexers/config/DocIndexerXPath.java
+++ core/src/main/java/nl/inl/blacklab/indexers/config/DocIndexerXPath.java
@@ -669,6 +669,11 @@ public class DocIndexerXPath extends DocIndexerConfig {
             navpush();
             AutoPilot apValuePath = acquireAutoPilot(valuePath);
             if (annotation.isMultipleValues()) {
+                // If we don't want duplicates, keep track of the values we've indexed
+                Set<String> valuesAlreadyIndexed = null;
+                if (!annotation.isAllowDuplicateValues())
+                    valuesAlreadyIndexed = new HashSet<>();
+                
                 // Multiple matches will be indexed at the same position.
                 AutoPilot apEvalToString = acquireAutoPilot(".");
                 boolean firstValue = true;
@@ -676,9 +681,17 @@ public class DocIndexerXPath extends DocIndexerConfig {
                     apEvalToString.resetXPath();
                     String annotValue = apEvalToString.evalXPathToString();
                     annotValue = processString(annotValue, annotation.getProcess(), null);
-                    int increment = firstValue ? 1 : 0;
-                    annotation(annotation.getName(), annotValue, increment, indexAtPositions);
-                    firstValue = false;
+                    if (valuesAlreadyIndexed == null || !valuesAlreadyIndexed.contains(annotValue)) {
+                        // We don't care about duplicates or we do and this is not a duplicate.
+                        // Index the value.
+                        int increment = firstValue ? 1 : 0;
+                        annotation(annotation.getName(), annotValue, increment, indexAtPositions);
+                        firstValue = false;
+                        if (valuesAlreadyIndexed != null) {
+                            // Keep track of values seen so we can discard duplicates
+                            valuesAlreadyIndexed.add(annotValue);
+                        }
+                    }
                 }
                 releaseAutoPilot(apEvalToString);
     
