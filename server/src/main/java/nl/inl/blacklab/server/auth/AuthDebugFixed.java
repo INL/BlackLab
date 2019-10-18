a66c037e7226418aba6a0e29c228779a5b3931b4
Koen Mertens <koen.mertens@ivdnt.org>
2019-10-18 15:44:28 +0200

    Always allow authDebugFixed for requests from a local ip

--- server/src/main/java/nl/inl/blacklab/server/auth/AuthDebugFixed.java
+++ server/src/main/java/nl/inl/blacklab/server/auth/AuthDebugFixed.java
@@ -1,13 +1,13 @@
 package nl.inl.blacklab.server.auth;
 
 import java.util.Map;
+import java.util.regex.Pattern;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
 import nl.inl.blacklab.server.BlackLabServer;
 import nl.inl.blacklab.server.jobs.User;
 import nl.inl.blacklab.server.search.SearchManager;
@@ -23,6 +23,13 @@ public class AuthDebugFixed {
     static final Logger logger = LogManager.getLogger(AuthDebugFixed.class);
 
     private String userId;
+    
+    /**
+     * 192.168.0.0 - 192.168.255.255
+     * 172.16.0.0 - 172.31.255.255 
+     * 10.0.0.0 - 10.255.255.255
+     */
+    private static final Pattern PATT_LOCALSUBNET = Pattern.compile("^(10\\.|172\\.(1[6-9]|2\\d|3[01])|192\\.168)");
 
     public AuthDebugFixed(Map<String, Object> parameters) {
         boolean hasUserId = parameters.containsKey("userId");
@@ -38,10 +45,12 @@ public class AuthDebugFixed {
 
         String sessionId = request.getSession().getId();
 
-        // Is client on debug IP?
+        // Is client on debug IP or on the local network?
         SearchManager searchMan = ((BlackLabServer) servlet).getSearchManager();
         String ip = request.getRemoteAddr();
-        if (!searchMan.config().getDebug().isDebugMode(ip) && !searchMan.config().getAuthentication().isOverrideIp(ip)) {
+        
+        boolean isLocalNetwork = PATT_LOCALSUBNET.matcher(ip).find();
+        if (!isLocalNetwork && !searchMan.config().getDebug().isDebugMode(ip) && !searchMan.config().getAuthentication().isOverrideIp(ip)) {
             return User.anonymous(sessionId);
         }
 
