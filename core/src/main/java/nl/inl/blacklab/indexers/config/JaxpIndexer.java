4dd74adc9cf7b44bf44feb0ebbc25fd047f70eb6
eduarddrenth <eduarddrenth@gmail.com>
2019-11-26 18:15:10 +0100

    zo ongeveer...

--- core/src/main/java/nl/inl/blacklab/indexers/config/JaxpIndexer.java
+++ core/src/main/java/nl/inl/blacklab/indexers/config/JaxpIndexer.java
@@ -1,9 +1,50 @@
 package nl.inl.blacklab.indexers.config;
 
+import net.sf.saxon.Configuration;
+import net.sf.saxon.om.TreeInfo;
+import net.sf.saxon.trans.XPathException;
+import net.sf.saxon.xpath.XPathFactoryImpl;
+import nl.inl.blacklab.exceptions.BlackLabRuntimeException;
+import nl.inl.blacklab.index.Indexer;
+import org.apache.commons.io.IOUtils;
+import org.xml.sax.InputSource;
+
+import javax.xml.transform.sax.SAXSource;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathFactory;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * An indexer configured using full XPath expressions.
  */
 public abstract class JaxpIndexer extends DocIndexerConfig {
+
+    private final XPathFactory xpFactory = new XPathFactoryImpl();
+    private final XPath xpExpression = xpFactory.newXPath();
+
+    private TreeInfo treeInfo = null;
+    private Map<Integer, Integer> cumulativeColsPerLine = new HashMap<>();
+
+    @Override
+    public void setDocument(Reader reader) {
+        try {
+            byte[] bytes = IOUtils.toByteArray(reader, Indexer.DEFAULT_INPUT_ENCODING);
+            ByteArrayInputStream stream = new ByteArrayInputStream(bytes);
+            AtomicInteger line = new AtomicInteger();
+            IOUtils.lineIterator(stream, Indexer.DEFAULT_INPUT_ENCODING).forEachRemaining(l ->
+                    cumulativeColsPerLine.put(line.getAndIncrement(), l.length()));
+            stream.reset();
+            InputSource inputSrc = new InputSource(stream);
+            SAXSource saxSrc = new SAXSource(inputSrc);
+            Configuration config = ((XPathFactoryImpl) xpFactory).getConfiguration();
+            treeInfo = config.buildDocumentTree(saxSrc);
+        } catch (IOException | XPathException e) {
+            throw BlackLabRuntimeException.wrap(e);
+        }
+    }
 
     /*
 
