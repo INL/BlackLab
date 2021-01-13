INL/BlackLab

commit c857c7186d186cc9ddad4273cc911e58907b7aad

Fix broken csv by stripping newlines from metadata values

server/src/main/java/nl/inl/blacklab/server/requesthandlers/RequestHandlerDocsCsv.java
@@ -269,12 +269,8 @@ public class RequestHandlerDocsCsv extends RequestHandler {
                         }
                         sb.append('"');
                         while (offset < strlen) {
-                            int codepoint = value.codePointAt(offset);
-                            offset += Character.charCount(codepoint);
-                            sb.appendCodePoint(codepoint);
-                            if (codepoint == '"') {
-                                sb.appendCodePoint(codepoint);
-                            }
+                            // Strip newlines, modify quotes to be double quotes.
+                            value.replace("\n", "").replace("\r", "").replace("\"", "\"\"");
                         }
 
                         sb.append('"');

commit 724f76142d92733a79326548e9a029e71f7d9ba7

Fix infinite loop when exporting csv (hits and docs)
Introduced in c857c7186d186cc9ddad4273cc911e58907b7aad

server/src/main/java/nl/inl/blacklab/server/requesthandlers/RequestHandlerDocsCsv.java
@@ -267,17 +267,11 @@ public class RequestHandlerDocsCsv extends RequestHandler {
 
                     boolean firstValue = true;
                     for (String value : doc.getValues(fieldId)) {
-                        int offset = 0, strlen = value.length();
-
                         if (!firstValue) {
                             sb.append(" ");
                         }
                         sb.append('"');
-                        while (offset < strlen) {
-                            // Strip newlines, modify quotes to be double quotes.
-                            value.replace("\n", "").replace("\r", "").replace("\"", "\"\"");
-                        }
-
+                        sb.append(value.replace("\n", "").replace("\r", "").replace("\"", "\"\""));
                         sb.append('"');
                         firstValue = false;
                     }

server/src/main/java/nl/inl/blacklab/server/requesthandlers/RequestHandlerHitsCsv.java
@@ -358,21 +358,11 @@ public class RequestHandlerHitsCsv extends RequestHandler {
         StringBuilder sb = new StringBuilder();
         boolean firstValue = true;
         for (String value : strings) {
-            int offset = 0, strlen = value.length();
-
             if (!firstValue) {
                 sb.append(" ");
             }
             sb.append('"');
-            while (offset < strlen) {
-                int codepoint = value.codePointAt(offset);
-                offset += Character.charCount(codepoint);
-                sb.appendCodePoint(codepoint);
-                if (codepoint == '"') {
-                    sb.appendCodePoint(codepoint);
-                }
-            }
-
+            sb.append(value.replace("\n", "").replace("\r", "").replace("\"", "\"\""));
             sb.append('"');
             firstValue = false;
         }