6f9b3df3a899b2e0d527af72292c528ee4f114d3
Jan Niestadt <jan.niestadt@ivdnt.org>
2021-02-11 16:17:18 +0100

    Added a simple search test page to BLS: /test/
    This allows us to play around with parameters, easily disable cache and
    enable explain, etc.

---
+++ server/src/main/webapp/test/js/search.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+	
+	$("#zoekForm").submit(function(event) {
+		event.preventDefault();
+		
+		// Get parameters from form inputs
+        let param = {};
+		const paramsFromFields = [
+		  'patt', 'filter', 'first', 'number', 'wordsaroundhit', 'sort', 'group', 'usecache', 'explain', 'waitfortotal'
+		];
+		paramsFromFields.forEach(name => param[name] = $(`#${name}`).val());
+		
+		// Execute search
+		$("#wait").text("Executing search...");
+		const url = $("#corpusUrl").val() + "/hits";
+		performAjaxSearchRequest(url, param, function (results) {
+		
+		  // Show results
+          $("#wait").hide();
+          delete results.summary.docFields;
+          delete results.summary.metadataFieldDisplayNames;
+		  $("#output").text("Results summary: (use browser tools to see full response)\n\n" + JSON.stringify(results.summary, null, 2)).show();
+		  
+		}, false, null);
+	});
+});
