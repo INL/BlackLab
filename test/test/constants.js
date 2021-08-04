6a4ee57279f3153b143329193b293f64e147aa36
Jan Niestadt <jan.niestadt@ivdnt.org>
2021-08-04 15:32:17 +0200

    Work on integration tests.

---
+++ test/test/constants.js
@@ -0,0 +1,7 @@
+const SERVER_URL = process.env.APP_URL || "http://localhost:8080/blacklab-server";
+const DEFAULT_WINDOW_SIZE = 50;
+
+module.exports = {
+    SERVER_URL,
+    DEFAULT_WINDOW_SIZE,
+};
