2b96e11ce1362e824bd61daeb1c7f3a22a4a3305
Jan Niestadt <jan.niestadt@ivdnt.org>
2022-02-21 12:58:31 +0100

    Add test/run.sh script.

---
+++ test/run.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Build and run BlackLab Server
+# (--force-recreate to avoid error 'network not found')
+docker-compose up --force-recreate -d --build testserver
+
+# Build and run the test suite
+docker-compose build test
+docker-compose run --rm test
+
+# Clean up
+# (stop then down to avoid warning about network in use)
+docker-compose stop testserver
+docker-compose down testserver
