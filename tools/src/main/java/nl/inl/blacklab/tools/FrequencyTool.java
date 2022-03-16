0afe49aa581cde09c70810cdf686b1023824a9d5
Jan Niestadt <jan.niestadt@ivdnt.org>
2022-03-16 14:33:10 +0100

    WIP.

--- tools/src/main/java/nl/inl/blacklab/tools/FrequencyTool.java
+++ tools/src/main/java/nl/inl/blacklab/tools/FrequencyTool.java
@@ -1,10 +1,16 @@
 package nl.inl.blacklab.tools;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
+import com.fasterxml.jackson.core.JsonEncoding;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
@@ -14,11 +20,14 @@ import nl.inl.blacklab.exceptions.InvalidQuery;
 import nl.inl.blacklab.resultproperty.HitProperty;
 import nl.inl.blacklab.resultproperty.HitPropertyHitText;
 import nl.inl.blacklab.resultproperty.HitPropertyMultiple;
+import nl.inl.blacklab.resultproperty.PropertyValue;
+import nl.inl.blacklab.resultproperty.PropertyValueMultiple;
 import nl.inl.blacklab.search.BlackLab;
 import nl.inl.blacklab.search.BlackLabIndex;
 import nl.inl.blacklab.search.indexmetadata.AnnotatedField;
 import nl.inl.blacklab.search.indexmetadata.Annotation;
 import nl.inl.blacklab.search.lucene.SpanQueryAnyToken;
+import nl.inl.blacklab.search.results.HitGroup;
 import nl.inl.blacklab.search.results.HitGroups;
 import nl.inl.blacklab.search.results.QueryInfo;
 import nl.inl.blacklab.searches.SearchHitGroups;
@@ -156,29 +165,30 @@ public class FrequencyTool {
         if (args.length > 2) {
             outputDir = new File(args[2]);
         }
-        if (!indexDir.isDirectory() || !indexDir.canWrite()) {
+        if (!outputDir.isDirectory() || !outputDir.canWrite()) {
             exit("Not a directory or cannot write to output dir " + outputDir);
         }
 
         // Generate the frequency lists
-        makeFrequencyLists(index, annotatedField, config.getFrequencyLists());
+        makeFrequencyLists(index, annotatedField, config.getFrequencyLists(), outputDir);
     }
 
     private static void makeFrequencyLists(BlackLabIndex index, AnnotatedField annotatedField,
-                                           List<ConfigFreqList> freqLists) {
+                                           List<ConfigFreqList> freqLists, File outputDir) {
         for (ConfigFreqList freqList: freqLists) {
-            makeFrequencyList(index, annotatedField, freqList);
+            makeFrequencyList(index, annotatedField, freqList, outputDir);
         }
     }
 
-    private static void makeFrequencyList(BlackLabIndex index, AnnotatedField annotatedField, ConfigFreqList freqList) {
+    private static void makeFrequencyList(BlackLabIndex index, AnnotatedField annotatedField,
+                                          ConfigFreqList freqList, File outputDir) {
         QueryInfo queryInfo = QueryInfo.create(index);
         SpanQueryAnyToken anyToken = new SpanQueryAnyToken(queryInfo, 1, 1, annotatedField.name());
         HitProperty groupBy = getGroupBy(index, annotatedField, freqList);
         SearchHitGroups search = index.search().find(anyToken).groupStats(groupBy, 0);
         try {
             HitGroups result = search.executeNoQueue();
-            writeOutput(index, annotatedField, freqList, result);
+            writeOutput(index, annotatedField, freqList, result, outputDir);
         } catch (InvalidQuery e) {
             throw new BlackLabRuntimeException("Error creating freqList " + freqList.getName(), e);
         }
@@ -193,10 +203,75 @@ public class FrequencyTool {
         return new HitPropertyMultiple(groupProps.toArray(new HitProperty[0]));
     }
 
-    private static void writeOutput(BlackLabIndex index, AnnotatedField annotatedField, ConfigFreqList freqList, HitGroups result) {
-        File outputFile = new File(freqList.getName() + ".json");
+    private static String currentDateTime() {
+        final String DATE_FORMAT_NOW = "yyyy-MM-dd HH:mm:ss";
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT_NOW);
+        return sdf.format(cal.getTime());
+    }
 
-        
+    private static void writeOutput(BlackLabIndex index, AnnotatedField annotatedField, ConfigFreqList freqList,
+                                    HitGroups result, File outputDir) {
+        File outputFile = new File(outputDir, freqList.getName() + ".json");
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        JsonFactory jfactory = new JsonFactory();
+        try {
+            JsonGenerator j = jfactory.createGenerator(stream, JsonEncoding.UTF8);
+            j.writeStartObject();
+            {
+                j.writeStringField("generatedAt", currentDateTime());
+                j.writeStringField("indexName", index.name());
+                j.writeStringField("annotatedField", annotatedField.name());
+                j.writeFieldName("config"); writeOutputConfig(j, freqList);
+                j.writeFieldName("results"); writeOutputResults(j, result);
+            }
+            j.writeEndObject();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void writeOutputResults(JsonGenerator j, HitGroups groups) throws IOException {
+        j.writeStartArray();
+        for (HitGroup group: groups) {
+            j.writeStartObject();
+            {
+                PropertyValue identity = group.identity();
+                j.writeFieldName("identity");
+                j.writeStartArray();
+                {
+                    if (identity instanceof PropertyValueMultiple) {
+                        // Grouped by multiple properties. Serialize each value separately
+                        PropertyValueMultiple values = (PropertyValueMultiple) identity;
+                        for (PropertyValue value: values.values()) {
+                            j.writeString(value.toString());
+                        }
+                    } else {
+                        // Grouped by single property. Serialize it.
+                        j.writeString(identity.toString());
+                    }
+                }
+                j.writeEndArray();
+                j.writeIntField("size", group.size());
+            }
+            j.writeEndObject();
+        }
+        j.writeEndArray();
+    }
+
+    private static void writeOutputConfig(JsonGenerator j, ConfigFreqList freqList) throws IOException {
+        j.writeStartObject();
+        {
+            j.writeStringField("name", freqList.getName());
+            j.writeFieldName("annotations"); writeOutputList(j, freqList.getAnnotations());
+            j.writeFieldName("metadataFields"); writeOutputList(j, freqList.getMetadataFields());
+        }
+        j.writeEndObject();
+    }
+
+    private static void writeOutputList(JsonGenerator j, List<String> l) throws IOException {
+        String[] arr = l.toArray(new String[0]);
+        j.writeArray(arr, 0, arr.length);
     }
 
 }
