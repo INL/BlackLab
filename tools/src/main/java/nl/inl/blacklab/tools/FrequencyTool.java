429152671b061402cc0de2f4985d529b56e60d76
Jan Niestadt <jan.niestadt@ivdnt.org>
2022-03-16 15:06:27 +0100

    Work on FrequencyTool.

--- tools/src/main/java/nl/inl/blacklab/tools/FrequencyTool.java
+++ tools/src/main/java/nl/inl/blacklab/tools/FrequencyTool.java
@@ -1,8 +1,9 @@
 package nl.inl.blacklab.tools;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -18,6 +19,7 @@ import nl.inl.blacklab.exceptions.BlackLabRuntimeException;
 import nl.inl.blacklab.exceptions.ErrorOpeningIndex;
 import nl.inl.blacklab.exceptions.InvalidQuery;
 import nl.inl.blacklab.resultproperty.HitProperty;
+import nl.inl.blacklab.resultproperty.HitPropertyDocumentStoredField;
 import nl.inl.blacklab.resultproperty.HitPropertyHitText;
 import nl.inl.blacklab.resultproperty.HitPropertyMultiple;
 import nl.inl.blacklab.resultproperty.PropertyValue;
@@ -26,6 +28,7 @@ import nl.inl.blacklab.search.BlackLab;
 import nl.inl.blacklab.search.BlackLabIndex;
 import nl.inl.blacklab.search.indexmetadata.AnnotatedField;
 import nl.inl.blacklab.search.indexmetadata.Annotation;
+import nl.inl.blacklab.search.lucene.BLSpanQuery;
 import nl.inl.blacklab.search.lucene.SpanQueryAnyToken;
 import nl.inl.blacklab.search.results.HitGroup;
 import nl.inl.blacklab.search.results.HitGroups;
@@ -73,6 +76,7 @@ public class FrequencyTool {
             return frequencyLists;
         }
 
+        @SuppressWarnings("unused")
         public void setFrequencyLists(List<ConfigFreqList> frequencyLists) {
             this.frequencyLists = frequencyLists;
         }
@@ -150,27 +154,27 @@ public class FrequencyTool {
         if (!indexDir.isDirectory() || !indexDir.canRead()) {
             exit("Can't read or not a directory " + indexDir);
         }
-        BlackLabIndex index = BlackLab.open(indexDir);
+        try (BlackLabIndex index = BlackLab.open(indexDir)) {
+            // Read config
+            File configFile = new File(args[1]);
+            if (!configFile.canRead()) {
+                exit("Can't read config file " + configFile);
+            }
+            Config config = Config.fromFile(configFile);
+            AnnotatedField annotatedField = index.annotatedField(config.getAnnotatedField());
 
-        // Read config
-        File configFile = new File(args[1]);
-        if (!configFile.canRead()) {
-            exit("Can't read config file " + configFile);
-        }
-        Config config = Config.fromFile(configFile);
-        AnnotatedField annotatedField = index.annotatedField(config.getAnnotatedField());
+            // Output dir
+            File outputDir = new File(System.getProperty("user.dir")); // current dir
+            if (args.length > 2) {
+                outputDir = new File(args[2]);
+            }
+            if (!outputDir.isDirectory() || !outputDir.canWrite()) {
+                exit("Not a directory or cannot write to output dir " + outputDir);
+            }
 
-        // Output dir
-        File outputDir = new File(System.getProperty("user.dir")); // current dir
-        if (args.length > 2) {
-            outputDir = new File(args[2]);
+            // Generate the frequency lists
+            makeFrequencyLists(index, annotatedField, config.getFrequencyLists(), outputDir);
         }
-        if (!outputDir.isDirectory() || !outputDir.canWrite()) {
-            exit("Not a directory or cannot write to output dir " + outputDir);
-        }
-
-        // Generate the frequency lists
-        makeFrequencyLists(index, annotatedField, config.getFrequencyLists(), outputDir);
     }
 
     private static void makeFrequencyLists(BlackLabIndex index, AnnotatedField annotatedField,
@@ -183,7 +187,7 @@ public class FrequencyTool {
     private static void makeFrequencyList(BlackLabIndex index, AnnotatedField annotatedField,
                                           ConfigFreqList freqList, File outputDir) {
         QueryInfo queryInfo = QueryInfo.create(index);
-        SpanQueryAnyToken anyToken = new SpanQueryAnyToken(queryInfo, 1, 1, annotatedField.name());
+        BLSpanQuery anyToken = new SpanQueryAnyToken(queryInfo, 1, 1, annotatedField.name());
         HitProperty groupBy = getGroupBy(index, annotatedField, freqList);
         SearchHitGroups search = index.search().find(anyToken).groupStats(groupBy, 0);
         try {
@@ -196,9 +200,15 @@ public class FrequencyTool {
 
     private static HitProperty getGroupBy(BlackLabIndex index, AnnotatedField annotatedField, ConfigFreqList freqList) {
         List<HitProperty> groupProps = new ArrayList<>();
+        // Add annotations to group by
         for (String name: freqList.getAnnotations()) {
             Annotation annotation = annotatedField.annotation(name);
             groupProps.add(new HitPropertyHitText(index, annotation));
+        }
+        // Add metadata fields to group by
+        for (String name: freqList.getMetadataFields()) {
+            Annotation annotation = annotatedField.annotation(name);
+            groupProps.add(new HitPropertyDocumentStoredField(index, name));
         }
         return new HitPropertyMultiple(groupProps.toArray(new HitProperty[0]));
     }
@@ -213,21 +223,23 @@ public class FrequencyTool {
     private static void writeOutput(BlackLabIndex index, AnnotatedField annotatedField, ConfigFreqList freqList,
                                     HitGroups result, File outputDir) {
         File outputFile = new File(outputDir, freqList.getName() + ".json");
-        ByteArrayOutputStream stream = new ByteArrayOutputStream();
-        JsonFactory jfactory = new JsonFactory();
-        try {
-            JsonGenerator j = jfactory.createGenerator(stream, JsonEncoding.UTF8);
-            j.writeStartObject();
-            {
-                j.writeStringField("generatedAt", currentDateTime());
-                j.writeStringField("indexName", index.name());
-                j.writeStringField("annotatedField", annotatedField.name());
-                j.writeFieldName("config"); writeOutputConfig(j, freqList);
-                j.writeFieldName("results"); writeOutputResults(j, result);
+        try (OutputStream stream = new FileOutputStream(outputFile)) {
+            JsonFactory jfactory = new JsonFactory();
+            try (JsonGenerator j = jfactory.createGenerator(stream, JsonEncoding.UTF8)) {
+                j.writeStartObject();
+                {
+                    j.writeStringField("generatedAt", currentDateTime());
+                    j.writeStringField("indexName", index.name());
+                    j.writeStringField("annotatedField", annotatedField.name());
+                    j.writeFieldName("config");
+                    writeOutputConfig(j, freqList);
+                    j.writeFieldName("results");
+                    writeOutputResults(j, result);
+                }
+                j.writeEndObject();
             }
-            j.writeEndObject();
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Error writing output for " + freqList.getName(), e);
         }
     }
 
@@ -252,7 +264,7 @@ public class FrequencyTool {
                     }
                 }
                 j.writeEndArray();
-                j.writeIntField("size", group.size());
+                j.writeNumberField("size", group.size());
             }
             j.writeEndObject();
         }
@@ -263,8 +275,10 @@ public class FrequencyTool {
         j.writeStartObject();
         {
             j.writeStringField("name", freqList.getName());
-            j.writeFieldName("annotations"); writeOutputList(j, freqList.getAnnotations());
-            j.writeFieldName("metadataFields"); writeOutputList(j, freqList.getMetadataFields());
+            j.writeFieldName("annotations");
+                writeOutputList(j, freqList.getAnnotations());
+            j.writeFieldName("metadataFields");
+                writeOutputList(j, freqList.getMetadataFields());
         }
         j.writeEndObject();
     }
