5bf099160a53a7049e10ce52d474c5843cca458a
Esteban Ginez <175813+eginez@users.noreply.github.com>
2022-03-21 07:59:36 -0700

    Integrations Tests Improvements (#280)
    Adds integration tests improvements, allows the execution of tests via
    alternative data and configurations.

--- test/README.md
+++ test/README.md
@@ -9,14 +9,20 @@ This is a work in progress. Not all functionality is yet covered by a test.
 
 ## How to use
 
-The easiest way to do this is to run this process in Docker; this automates the process and saves you from having to install (a specific version of) `npm` on your machine. From the top level of this repository, run:
+The easiest way to do this is to run this process in Docker; this automates the process and saves you from having to install (a specific version of) `npm` on your machine. From the top level of this repository run:
 
 ```bash
-# Run the tests
-docker-compose run --rm test
+## Optionally point to a directory containing custom blacklab server configurations
+#export CONFIG_ROOT=./custom-server-configuration
+
+## Optionally point to a directory containing custom data, optionally including a custom environment for testing
+#export INDEX_TEST_DATA_CONFIGURATION=./custom-data
+
+sh test/run.sh
 ```
 
-NOTE: if you've made changes and want to ensure the latest BlackLab code and tests are being used:
+### Build a blacklab test server
+If you've made changes ensure the latest BlackLab code and tests are being used by:
 
 ```bash
 # Ensure testserver is recreated (if it changed)
@@ -24,7 +30,33 @@ docker-compose up -d --build testserver
 
 # Ensure the test image is up to date
 docker-compose build test
+```
 
+The blacklab server under test uses configurations files defined in the `docker` directory. If you would like to override the 
+default configurations when building the `testserver` use the `CONFIG_ROOT` build argument pointing to
+a directory containing:
+- server.xml: will be used to configure tomcat
+- blacklab-server.xml: will be used to configure blacklab
+
+and build the test server again.
+
+NOTE: `CONFIG_ROOT` must be a path residing *inside the repository file structure and relative to the root of the repository*
+
+```bash
+# PATH_TO_CUSTOM_CONFIG must contain at least two files: blacklab-server.xml and server.xml
+CONFIG_ROOT=[PATH_TO_CUSTOM_CONFIG] docker-compose up -d --build testserver
+```
+
+### Build and run the tests
+Build the containers with the tests
+```bash
+## Build the tests container
+docker-compose build test
+```
+By default, it will use the data found the `test/data` directory for testing
+
+Run the tests
+```bash
 # Run the tests
 docker-compose run --rm test
 ```
@@ -37,6 +69,19 @@ If you want to run the tests manually outside of Docker, you should:
 - start a local server that can access this index
 - run the tests using `npm run test` from the `test` directory
 
+### Custom test configuration
+Blacklab allows to configure the shape of the responses, to allow for the tests to account for different 
+server configurations, the tests can also be configured via enviroment variables matching the server
+configurations. Consult the [constants.js](test/constants.js) for the list of supported environment variables.
+
+You can easily override the defaults by setting the environment them within a file name `environment` and building/running
+the test docker container. See the [below section on custom data]
+
+```bash
+echo BLACKLAB_DEFAULT_WINDOW_SIZE=20 >> test/data/environment
+docker-compose build test
+docker-compose run --rm test
+```
 
 ## Tools
 
@@ -47,8 +92,31 @@ Mocha/Chai were chosen because they are mature, popular and tests are easy to un
 
 ## Test data
 
+### Default data
 We currently use a small sample of data from the [VOICE](https://www.univie.ac.at/voice/) (Vienna-Oxford International Corpus of English) project. The test data consists of lemmatized and PoS-tagged TEI, making it easy to index in BlackLab. Using English also makes it easier for others to read and write tests.
 
 The test data is included in our repository (in the `test/data` subdirectory) because the tests rely on this specific data, so it is important that they can be distributed together.
 
 Data from the VOICE project is licensed under the [Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License](http://creativecommons.org/licenses/by-nc-sa/3.0/). As BlackLab is noncommercial and licensed under the Apache license, we believe that including this test data conforms to this license. If you are the rightsholder and believe otherwise, please let us know.
+
+### Custom data
+
+You can customize the testing data used for **indexing tests** by setting the `INDEX_TEST_DATA_ROOT` to a directory
+containing test data configuration files. `INDEX_TEST_DATA_ROOT` must available inside the repository and
+relative to the `test/` directory.
+
+In addition, if the `INDEX_TEST_DATA_ROOT` directory contains a file named `environment`. The [test script](run-tests.sh)
+will source it before running any tests. This allows the test to add environment variables to control for custom blacklab server configurations.
+
+For guidance on how to configure custom test data see the example here: [index-test-config.json](data/index-test-config.json)
+
+
+## GithubActions to run tests on pull requests and merges
+
+The blacklab repository automates pull request checks via github actions jobs.
+There are currently two workflows ensuring the quality of the pull requests:
+
+- [Build and unit tests](../.github/workflows/maven.yml): builds the code change and runs all blacklab unit tests via maven.
+- [Integration tests](../.github/workflows/integration-test.yml): builds the code change and runs [integration tests](./test).
+  Note: Integrations tests are currently running on two different data sets, [the default data set](./test/data) and a 
+  custom configured data set whose settings are recorded on a per-repository basis.
---
+++ test/test/indices.js
@@ -0,0 +1,237 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const expect = chai.expect;
+const should = chai.should();
+const assert = chai.assert;
+const crypto = require('crypto')
+const fs = require('fs')
+const path = require('path')
+const parseXmlString = require('xml2js').parseStringPromise;
+chai.use(chaiHttp);
+
+const constants = require('./constants');
+const SERVER_URL = constants.SERVER_URL;
+
+const TEST_DATA_ROOT =  constants.INDEX_TEST_DATA_ROOT;
+const TEST_CONFIG = JSON.parse(fs.readFileSync(path.resolve(TEST_DATA_ROOT, 'index-test-config.json')));
+
+const INPUT_FORMAT_PATH = path.resolve(TEST_DATA_ROOT, TEST_CONFIG['input-format']);
+const DOC_TO_INDEX_PATH = path.resolve(TEST_DATA_ROOT, TEST_CONFIG['docs-to-index']);
+const EXPECTED_INDEX_CONTENT_PATH = path.resolve(TEST_DATA_ROOT, TEST_CONFIG['expected-index']);
+const EXPECTED_INDEX_METADATA_PATH = path.resolve(TEST_DATA_ROOT, TEST_CONFIG['expected-metadata']);
+
+function addDefaultHeaders(request) {
+    request.auth(constants.BLACKLAB_USER, constants.BLACKLAB_PASSWORD)
+    let allHeaders = {
+        'X-Request-ID': crypto.randomBytes(8).toString('hex'),
+    }
+
+    for (let key in allHeaders) {
+        request.set(key, allHeaders[key]);
+    }
+}
+
+function createIndexName() {
+    indexName = "test-index-" + crypto.randomInt(10000).toString();
+    return indexName;
+}
+
+async function createInputFormat(){
+    var formatName = path.basename(INPUT_FORMAT_PATH);
+    let request = chai
+        .request(SERVER_URL)
+        .post('/input-formats')
+        .set('Accept', 'application/json')
+        .attach('data', fs.readFileSync(INPUT_FORMAT_PATH), formatName);
+    addDefaultHeaders(request);
+    return request;
+}
+
+async function createIndex(indexName) {
+    index_url = constants.BLACKLAB_USER + ":" + indexName
+    inputFormat = TEST_CONFIG['input-format'].split('.')[0];
+    let request = chai
+        .request(SERVER_URL)
+        .post('/')
+        .query({
+            'name': index_url,
+            'display': indexName,
+            'format': constants.BLACKLAB_USER + ":" + inputFormat
+        })
+        .set('Accept', 'application/json')
+    addDefaultHeaders(request);
+    return request.send();
+}
+
+async function getIndexRequest(indexName) {
+    indexUrl = constants.BLACKLAB_USER + ":" + indexName
+    let request = chai
+        .request(SERVER_URL)
+        .get("/" + indexUrl + "/status")
+        .set('Accept', 'application/json')
+    addDefaultHeaders(request);
+    return request.send();
+}
+
+async function addToIndex(indexName, payloadPath) {
+    indexUrl = constants.BLACKLAB_USER + ":" + indexName
+    let request = chai
+        .request(SERVER_URL)
+        .post('/' + indexUrl + '/docs')
+        .set('Accept', 'application/json')
+        .attach('data', fs.readFileSync(payloadPath), 'testdocs' );
+    addDefaultHeaders(request);
+    return request;
+}
+async function getIndexContent(indexName) {
+    indexUrl = constants.BLACKLAB_USER + ":" + indexName;
+    let request = chai
+        .request(SERVER_URL)
+        .get("/" + indexUrl + "/docs")
+        .set('Accept', 'application/json')
+    addDefaultHeaders(request);
+    return request.send();
+}
+
+function clearKeys(keys, data) {
+    var diff = Object.keys(data).filter(k => !keys.includes(k));
+    var cleanedData = {}
+    for (let k of diff) {
+        var value = data[k];
+        if (!(value instanceof Array)) {
+            cleanedData[k] = value;
+            continue;
+        }
+        if (value.length === 1 && typeof value[0] === "string" && (value[0].trim() === "" || value[0].trim() === "\n")) {
+            cleanedData[k] = [];
+        } else {
+            cleanedData[k] = value;
+        }
+    }
+    return cleanedData
+}
+
+async function getIndexMetadata(indexName) {
+    indexUrl = constants.BLACKLAB_USER + ":" + indexName
+    let request = chai
+        .request(SERVER_URL)
+        .get("/" + indexUrl + "/")
+        .set('Accept', 'application/json')
+    addDefaultHeaders(request);
+    return request.send();
+}
+
+async function toJson(xml) {
+    return parseXmlString(xml);
+}
+
+function queryIndex(indexName, pattern, filters, format = 'application/json') {
+    indexUrl = constants.BLACKLAB_USER + ":" + indexName + "/hits";
+    var respFormat = format === "" ? "application/json" : format;
+    let request = chai
+        .request(SERVER_URL)
+        .post("/" + indexUrl + "/")
+        .buffer()
+        .set('Accept', respFormat)
+    if (filters !== "") {
+        request.query({"patt": pattern, "filter": filters})
+    } else {
+        request.query({"patt": pattern})
+    }
+    addDefaultHeaders(request);
+    request.parse((res, callback) => {
+        res.text = '';
+        res.on('data', (chk) => {
+            res.text += chk;
+        });
+        res.on('end', () => callback(null, res.text));
+    });
+    return request.send()
+}
+
+
+describe('Indexing tests', () => {
+    it('create a new index', async () => {
+        indexName = createIndexName();
+        let respFormat = await createInputFormat();
+        assert.isTrue(respFormat.ok);
+
+        let createRes = await createIndex(indexName);
+        assert.isTrue(createRes.ok);
+
+        let resGetIndex = await getIndexRequest(indexName);
+        assert.isTrue(resGetIndex.ok);
+    });
+    it('adds to index', async () => {
+        indexName = createIndexName();
+        const req = await createInputFormat();
+        assert.isTrue(req.ok);
+
+        let createRes = await createIndex(indexName);
+        assert.isTrue(createRes.ok);
+
+        let addReq = await addToIndex(indexName, DOC_TO_INDEX_PATH);
+        assert.isTrue(addReq.ok);
+        
+        let indexContents = await getIndexContent(indexName);
+        assert.isTrue(indexContents.ok);
+
+        var body = indexContents.body;
+        var expectedContent = JSON.parse(fs.readFileSync(EXPECTED_INDEX_CONTENT_PATH));
+
+        var keys = ['summary', 'searchTime'];
+        expect(clearKeys(keys, expectedContent)).to.be.deep.equal(clearKeys(keys, body));
+    });
+
+    it('get index metadata', async () => {
+        indexName = createIndexName();
+        await createInputFormat();
+
+        let createRes = await createIndex(indexName);
+        assert.isTrue(createRes.ok);
+
+        let addReq = await addToIndex(indexName, DOC_TO_INDEX_PATH);
+        assert.isTrue(addReq.ok);
+
+        let indexMetadata = await getIndexMetadata(indexName);
+        var body = indexMetadata.body;
+        assert.isTrue(indexMetadata.ok);
+
+        var expectedMetadata = JSON.parse(fs.readFileSync(EXPECTED_INDEX_METADATA_PATH));
+
+        var keys = ['indexName', 'displayName', 'versionInfo', 'documentFormat']
+        expect(clearKeys(keys, expectedMetadata)).to.be.deep.equal(clearKeys(keys, body));
+    });
+
+    it('query from config', async () => {
+        indexName = createIndexName();
+        await createInputFormat();
+
+        let createRes = await createIndex(indexName);
+        assert.isTrue(createRes.ok);
+
+        let addReq = await addToIndex(indexName, DOC_TO_INDEX_PATH);
+        assert.isTrue(addReq.ok);
+
+        var queriesTest = TEST_CONFIG['queries'];
+        for (let [testName, testCase]  of Object.entries(queriesTest)) {
+            console.log("Running test case: " + testName);
+            const filterTerms = testCase['filters'];
+            const queryInd = await queryIndex(indexName, '"120"', filterTerms, "application/xml")
+            assert.isTrue(queryInd.ok);
+            const body = await toJson(queryInd.body);
+
+            const keys = ['summary'];
+            if (testCase['expected'] === null) {
+                var results = clearKeys(keys, body['blacklabResponse']);
+                expect(results['hits']).to.be.empty;
+            } else {
+                var expectedOutput = await toJson(fs.readFileSync(path.resolve(TEST_DATA_ROOT, testCase['expected'])));
+                expect(clearKeys(keys, expectedOutput['blacklabResponse'])).to.be.deep.equal(clearKeys(keys, body['blacklabResponse']));
+            }
+        }
+    });
+
+
+});
+
