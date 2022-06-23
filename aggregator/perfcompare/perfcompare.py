04f00663f3c7410756a9a5911bce6140f950f71a
Jan Niestadt <jan.niestadt@ivdnt.org>
2022-06-23 13:19:31 +0200

    Perfcompare for regular hits searches.

--- aggregator/perfcompare/perfcompare.py
+++ aggregator/perfcompare/perfcompare.py
@@ -2,30 +2,31 @@
 # Prints tab-separated timing per server.
 # See README.md.
 
+import sys
 import urllib.request
 import time
 import json
 import statistics
 from urllib.parse import urlencode, quote_plus
 
-def timeToGroup(corpus_url, word, groupby, repeat):
+def timeRequest(corpus_url, repeat, request):
     """ Returns average time for a grouping query on a server.
 
     Will repeat a grouping query a number of times (after a single warmup query)
     and returns the average response time.
     """
-    payload = {
-        'patt': f'[word="{word}"]',
-        'group': groupby,
-        'usecache': 'no'
-    }
-    result = urlencode(payload, quote_via=quote_plus)
+    result = urlencode(request, quote_via=quote_plus)
     url = f'{corpus_url}/hits?{result}'
+    print(url)
     duration = []
     for i in range(repeat + 1):  # 1 extra for warmup
         start = time.time()
-        with urllib.request.urlopen(url) as response:
-           html = response.read()
+        try:
+            with urllib.request.urlopen(url) as response:
+               html = response.read()
+        except urllib.error.HTTPError as e:
+            print(f'HTTPError: {e.code}\nRESPONSE: {e.read().decode()}')
+            raise e
         d = round(time.time() - start, 2)
         if i > 0: # first is warmup
             duration.append(d)
@@ -33,29 +34,40 @@ def timeToGroup(corpus_url, word, groupby, repeat):
     # return average duration
     return round(statistics.mean(duration), 2)
 
-def timeToGroupCompare(corpora, word, groupby, repeat):
+def timeCompareRequests(corpora, repeat, req):
     """ Prints tab-separated average response time for a grouping query on different servers """
     duration = []
     for url in corpora:
-        duration.append(timeToGroup(url, word, groupby, repeat))
+        duration.append(timeRequest(url, repeat, req))
+        time.sleep(0.5)
     str_dur = "\t".join(map(str, duration))
-    print(f'{word}\t{str_dur}')
+    print(f'{req["patt"]}\t{str_dur}')
 
-def timeToGroupCompareWords(corpora, run):
+def executeRun(corpora, repeat, run):
     """ Performs comparisons of grouping queries and prints tab-separated results """
-    print(f'# group by {run["groupby"]}')
-    for word in run['words']:
-        timeToGroupCompare(corpora, word, run['groupby'], run['repeat'])
+    config = { key:run[key] for key in run if key not in ['words', 'patts'] }
+    print(f'# {config}')
+    if 'words' in run:
+        for word in run['words']:
+            request = { **config, 'patt': f'"{word}"' }
+            timeCompareRequests(corpora, repeat, request)
+    if 'patts' in run:
+        for patt in run['patts']:
+            request = { **config, 'patt': patt }
+            timeCompareRequests(corpora, repeat, request)
 
 def main():
+    if len(sys.argv) < 1:
+        print('Usage: perfcompare.py <config.json>')
+        sys.exit(1)
+
     # Read config file
-    with open('perfcompare.json') as f:
+    with open(sys.argv[1]) as f:
        config = json.load(f)
 
     # Perform the configured runs
     for run in config['runs']:
-        run['repeat'] = run['repeat'] if 'repeat' in run else config['repeat']
-        timeToGroupCompareWords(config['corpora'], run)
+        executeRun(config['corpora'], config['repeat'], run)
 
 if __name__ == '__main__':
     main()
