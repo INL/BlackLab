0883d408bb7a4bdfbb4981b46e52b1fc04f20f01
Jan Niestadt <jan.niestadt@ivdnt.org>
2022-07-05 15:10:31 +0200

    test: use compose plugin if docker-compose doesn't work.

--- test/run.sh
+++ test/run.sh
@@ -2,20 +2,27 @@
 
 set -o errexit  # Exit on error (set -e)
 
+# Check how to call Compose
+COMPOSE=docker-compose
+if ! command -v $COMPOSE &> /dev/null
+then
+    COMPOSE="docker compose"
+fi
+
 export DOCKER_BUILDKIT=1
 
 # Build and run BlackLab Server
 # (--force-recreate to avoid error 'network not found')
-docker-compose up --force-recreate -d --build testserver
+$COMPOSE up --force-recreate -d --build testserver
 
 # Build and run the test suite
-docker-compose build test
-docker-compose run --rm test
+$COMPOSE build test
+$COMPOSE run --rm test
 
 # Clean up
 # (stop then down to avoid warning about network in use)
-docker-compose stop testserver
-#docker-compose down -v
+$COMPOSE stop testserver
+#$COMPOSE down -v
 docker container rm blacklab_testserver_1
 docker volume rm blacklab_blacklab-data
 docker network rm blacklab_default
