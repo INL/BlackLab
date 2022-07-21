ead0d48ed5baf48f92ab628181e5ef931635369c
Jan Niestadt <jan.niestadt@ivdnt.org>
2022-07-21 11:11:33 +0200

    Don't use SlowCompositeReaderWrapper, UninvertingReader from Solr.
    This pulled in a bunch of libraries, ballooning the size of our release artifacts.

--- engine/src/main/java/nl/inl/blacklab/forwardindex/FiidLookupExternal.java
+++ engine/src/main/java/nl/inl/blacklab/forwardindex/FiidLookupExternal.java
@@ -10,7 +10,6 @@ import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.LeafReader;
 import org.apache.lucene.index.LeafReaderContext;
 import org.apache.lucene.index.NumericDocValues;
-import org.apache.solr.uninverting.UninvertingReader;
 
 import net.jcip.annotations.NotThreadSafe;
 import nl.inl.blacklab.exceptions.BlackLabRuntimeException;
@@ -57,12 +56,8 @@ public class FiidLookupExternal implements FiidLookup {
                 LeafReader r = rc.reader();
                 NumericDocValues numericDocValues = r.getNumericDocValues(fiidFieldName);
                 if (numericDocValues == null) {
-                    // Use UninvertingReader to simulate DocValues (slower)
                     // (should never happen)
-                    Map<String, UninvertingReader.Type> fields = new TreeMap<>();
-                    fields.put(fiidFieldName, UninvertingReader.Type.INTEGER_POINT);
-                    LeafReader uninv = UninvertingReader.wrap(r, fields::get);
-                    numericDocValues = uninv.getNumericDocValues(fiidFieldName);
+                    throw new UnsupportedOperationException("no DocValues available");
                 }
                 if (numericDocValues != null) {
                     cachedFiids.put(rc.docBase, numericDocValues);
--- engine/src/main/java/nl/inl/blacklab/resultproperty/DocPropertyAnnotatedFieldLength.java
+++ engine/src/main/java/nl/inl/blacklab/resultproperty/DocPropertyAnnotatedFieldLength.java
@@ -9,7 +9,6 @@ import org.apache.lucene.index.LeafReader;
 import org.apache.lucene.index.LeafReaderContext;
 import org.apache.lucene.index.NumericDocValues;
 import org.apache.lucene.search.Query;
-import org.apache.solr.uninverting.UninvertingReader;
 
 import nl.inl.blacklab.exceptions.BlackLabRuntimeException;
 import nl.inl.blacklab.search.BlackLabIndex;
@@ -61,11 +60,8 @@ public class DocPropertyAnnotatedFieldLength extends DocProperty {
                 LeafReader r = rc.reader();
                 NumericDocValues numericDocValues = r.getNumericDocValues(fieldName);
                 if (numericDocValues == null) {
-                    // Use UninvertingReader to simulate DocValues (slower)
-                    Map<String, UninvertingReader.Type> fields = new TreeMap<>();
-                    fields.put(fieldName, UninvertingReader.Type.INTEGER_POINT);
-                    LeafReader uninv = UninvertingReader.wrap(r, fields::get);
-                    numericDocValues = uninv.getNumericDocValues(fieldName);
+                    // (should never happen)
+                    throw new UnsupportedOperationException("no DocValues available");
                 }
                 if (numericDocValues != null) {
                     docValues.put(rc.docBase, LuceneUtil.cacher(numericDocValues));
