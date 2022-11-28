c3ff558e1760120e0e056bb7d9799d5dce17e76a
Jan Niestadt <jan.niestadt@ivdnt.org>
2022-11-28 15:41:02 +0100

    Additional redirect fixing (site).

--- site/docs/.vuepress/enhanceApp.js
+++ site/docs/.vuepress/enhanceApp.js
@@ -1,33 +1,48 @@
-export default ({ router }) => {
-  router.addRoutes([
-    { path: '/add-input-format.html', redirect: '/guide/how-to-configure-indexing.html' },
-    { path: '/blacklab-in-action.html', redirect: '/guide/#try-it-online' },
-    { path: '/blacklab-internals.html', redirect: '/development/#internals' },
-    { path: '/blacklab-server-different-languages.html', redirect: '/server/from-different-languages.html' },
-    { path: '/blacklab-server-overview.html', redirect: '/server/' },
-    { path: '/blog.html', redirect: '/development/archive/blog.html' },
-    { path: '/changelog.html', redirect: '/development/changelog.html' },
-    { path: '/configuration-files.html', redirect: '/server/configuration.html' },
-    { path: '/corpus-query-language.html', redirect: '/guide/corpus-query-language.html' },
-    { path: '/corpus-search-engine.html', redirect: '/guide' },
-    { path: '/downloads.html', redirect: '/development/downloads.html' },
-    { path: '/example-application.html', redirect: '/development/examples/example-application.html' },
-    { path: '/faq.html', redirect: '/guide/faq.html' },
-    { path: '/features.html', redirect: '/guide/#features' },
-    { path: '/file-formats.html', redirect: '/development/#internals' },
-    { path: '/getting-started.html', redirect: '/guide/getting-started.html' },
-    { path: '/how-to-configure-indexing.html', redirect: '/guide/how-to-configure-indexing.html' },
-    { path: '/improve-search-speed.html', redirect: '/server/vmtouch.html' },
-    { path: '/indexing-with-blacklab.html', redirect: '/guide/indexing-with-blacklab.html' },
-    { path: '/install-macos.html', redirect: '/server/install-macos.html' },
-    { path: '/learn.html', redirect: '/guide' },
-    { path: '/migration-guide.html', redirect: '/development/migration-guide.html' },
-    { path: '/newsletter.html', redirect: '/development/archive/newsletter.html' },
-    { path: '/plugins.html', redirect: '/development/customization/plugins.html' },
-    { path: '/query-tool.html', redirect: '/development/query-tool.html' },
-    { path: '/roadmap.html', redirect: '/guide/future-plans.html' },
-    { path: '/tsv-example.html', redirect: '/guide/tsv-example.html' },
-    { path: '/who-uses-blacklab.html', redirect: '/guide/who-uses-blacklab.html' },
-    { path: '/xpath_examples.html', redirect: '/guide/xpath-examples.html' },
-  ]);
+export default ({router}) => {
+
+    /*
+    // From https://github.com/vuejs/vuepress/issues/1803
+    // (fix redirect issues)
+    router.beforeResolve((to, _from, next) => {
+        const browserWindow = typeof window !== "undefined" ? window : null;
+        if (browserWindow && to.matched.length && to.matched[0].path !== '*' && to.redirectedFrom) {
+            browserWindow.location.href = to.fullPath;
+        } else {
+            next();
+        }
+    });
+
+    router.addRoutes([
+        {path: '/add-input-format.html', redirect: '/guide/how-to-configure-indexing.html'},
+        {path: '/blacklab-in-action.html', redirect: '/guide/#try-it-online'},
+        {path: '/blacklab-internals.html', redirect: '/development/#internals'},
+        {path: '/blacklab-server-different-languages.html', redirect: '/server/from-different-languages.html'},
+        {path: '/blacklab-server-overview.html', redirect: '/server/'},
+        {path: '/blog.html', redirect: '/development/archive/blog.html'},
+        {path: '/changelog.html', redirect: '/development/changelog.html'},
+        {path: '/configuration-files.html', redirect: '/server/configuration.html'},
+        {path: '/corpus-query-language.html', redirect: '/guide/corpus-query-language.html'},
+        {path: '/corpus-search-engine.html', redirect: '/guide'},
+        {path: '/downloads.html', redirect: '/development/downloads.html'},
+        {path: '/example-application.html', redirect: '/development/examples/example-application.html'},
+        {path: '/faq.html', redirect: '/guide/faq.html'},
+        {path: '/features.html', redirect: '/guide/#features'},
+        {path: '/file-formats.html', redirect: '/development/#internals'},
+        {path: '/getting-started.html', redirect: '/guide/getting-started.html'},
+        {path: '/how-to-configure-indexing.html', redirect: '/guide/how-to-configure-indexing.html'},
+        {path: '/improve-search-speed.html', redirect: '/server/vmtouch.html'},
+        {path: '/indexing-with-blacklab.html', redirect: '/guide/indexing-with-blacklab.html'},
+        {path: '/install-macos.html', redirect: '/server/install-macos.html'},
+        {path: '/learn.html', redirect: '/guide'},
+        {path: '/migration-guide.html', redirect: '/development/migration-guide.html'},
+        {path: '/newsletter.html', redirect: '/development/archive/newsletter.html'},
+        {path: '/plugins.html', redirect: '/development/customization/plugins.html'},
+        {path: '/query-tool.html', redirect: '/development/query-tool.html'},
+        {path: '/roadmap.html', redirect: '/guide/future-plans.html'},
+        {path: '/tsv-example.html', redirect: '/guide/tsv-example.html'},
+        {path: '/who-uses-blacklab.html', redirect: '/guide/who-uses-blacklab.html'},
+        {path: '/xpath_examples.html', redirect: '/guide/xpath-examples.html'},
+    ]);
+    */
+
 };
