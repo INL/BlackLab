2a6eb17f13c00bfc2266d62a056e2fd57c774969
Jan Niestadt <jan.niestadt@ivdnt.org>
2023-02-16 12:32:50 +0100

    Tweak Compose files, isolate test stuff in test/

--- test/testrunner.sh
+++ test/testrunner.sh
@@ -13,8 +13,8 @@ set -o errexit  # Exit on error (set -e)
 # Get the servicename (or default to "test", the regular CI test)
 SERVICE_NAME="${1:-test}"
 
-# Go to the repository root
-cd $( dirname -- "$0"; )/..
+# Go to the test dir
+cd $( dirname -- "$0"; )/
 
 # Check how to call Compose
 COMPOSE=docker-compose
@@ -23,24 +23,18 @@ then
     COMPOSE="docker compose"
 fi
 
+# Enable BuildKit
 export DOCKER_BUILDKIT=1
 
 #----------------------------------------------------------
 # Build and run BlackLab Server
-# (--force-recreate to avoid error 'network not found')
 echo === Testing classic index format...
+$COMPOSE build testserver "$SERVICE_NAME"
 export BLACKLAB_FEATURE_integrateExternalFiles=false
-$COMPOSE up -d --build --force-recreate testserver
-
-# Build and run the test suite
-$COMPOSE build "$SERVICE_NAME"
+$COMPOSE up -d --force-recreate testserver # (--force-recreate to avoid error 'network not found')
 $COMPOSE run --rm "$SERVICE_NAME"
-
-# Clean up
-# (stop then rm -v instead of down -v, otherwise we get an error about the volume being in use)
-$COMPOSE stop testserver
+$COMPOSE stop testserver # (stop then rm -v instead of down -v, otherwise we get an error about the volume being in use)
 $COMPOSE rm -fv testserver
-
 
 #----------------------------------------------------------
 # Re-run to test the other index format as well
@@ -48,28 +42,17 @@ echo === Testing integrated index format...
 export BLACKLAB_FEATURE_integrateExternalFiles=true
 $COMPOSE up -d testserver
 $COMPOSE run --rm "$SERVICE_NAME"
-
-# Clean up
-# (stop then rm -v instead of down -v, otherwise we get an error about the volume being in use)
-$COMPOSE stop testserver
+$COMPOSE stop testserver # (stop then rm -v instead of down -v, otherwise we get an error about the volume being in use)
 $COMPOSE rm -fv testserver
-
 
 ##----------------------------------------------------------
 ## Re-run the same tests using Solr+proxy
-cd proxy
-$COMPOSE up --force-recreate -d --build
-cd ..
-export APP_URL=http://host.docker.internal:8080/blacklab-server
+$COMPOSE build proxy solr "$SERVICE_NAME"
+$COMPOSE up --force-recreate -d proxy solr
+export APP_URL=http://proxy:8080/blacklab-server
 export CORPUS_NAME=test
 export SKIP_INDEXING_TESTS=true   # not yet implemented for Solr
-sleep 15 # allow a little time to start up
-$COMPOSE build "$SERVICE_NAME"
+sleep 15 # allow Solr a little time to start up
 $COMPOSE run --rm "$SERVICE_NAME"
-
-# Clean up
-# (stop then rm -v instead of down -v, otherwise we get an error about the volume being in use)
-cd proxy
-$COMPOSE stop
+$COMPOSE stop # (stop then rm -v instead of down -v, otherwise we get an error about the volume being in use)
 $COMPOSE rm -fv
-cd ..
