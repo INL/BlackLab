04b7f81749af71aeed5518983f7719f6288d2fc4
Jan Niestadt <jan.niestadt@ivdnt.org>
2023-03-02 12:28:43 +0100

    Better handle error opening index.
    Made ErrorOpeningIndex/IndexVersionMismatch derive from BlackLabRuntimeException.
    Eliminated BlsIndexOpenException as we can catch above exceptions explicitly and
    don't need to duplicate exceptions for BLS.
    
    Made sure the server info request doesn't log long stack traces if some of your
    corpora are too old or new to be opened by this BlackLab version. It just logs a
    warning instead.

--- wslib/src/main/java/nl/inl/blacklab/server/exceptions/BlsException.java
+++ wslib/src/main/java/nl/inl/blacklab/server/exceptions/BlsException.java
@@ -4,10 +4,6 @@ package nl.inl.blacklab.server.exceptions;
  * Thrown when the requested index was not available or could not be opened
  */
 public class BlsException extends RuntimeException {
-    
-    public static InternalServerError indexVersionMismatch(Throwable e) {
-        return new BlsIndexOpenException("This index version cannot be opened with this BlackLab version.", "INTERR_INDEX_VERSION", e);
-    }
 
     /**
      * The HTTP error code to send.
