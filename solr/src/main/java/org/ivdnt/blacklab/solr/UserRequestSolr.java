b74abbb28ca1c93070c60edf57e1bc309a85bead
Jan Niestadt <jan.niestadt@ivdnt.org>
2023-03-06 12:39:40 +0100

    Correctly deal with cases with no index, e.g. input-formats.

--- solr/src/main/java/org/ivdnt/blacklab/solr/UserRequestSolr.java
+++ solr/src/main/java/org/ivdnt/blacklab/solr/UserRequestSolr.java
@@ -111,7 +111,8 @@ public class UserRequestSolr implements UserRequest {
         if (params.getDocumentFilterQuery().isEmpty()) {
             // No explicit bl.filter specified; use Solr's document results as our filter query
             DocSet docSet = rb.getResults() != null ? rb.getResults().docSet : null;
-            params.setFilterQuery(new DocSetFilter(docSet, index.metadata().metadataDocId()));
+            if (docSet != null && index != null)
+                params.setFilterQuery(new DocSetFilter(docSet, index.metadata().metadataDocId()));
         }
         return params;
     }
