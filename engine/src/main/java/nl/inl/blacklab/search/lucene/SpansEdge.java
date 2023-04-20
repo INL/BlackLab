c9ab62d0a48b9d7779f0e491f23c0dd5a5479d31
Jan Niestadt <jan.niestadt@ivdnt.org>
2023-04-20 09:34:44 +0200

    Update SpansEdge to be RTL-sensitive.

--- engine/src/main/java/nl/inl/blacklab/search/lucene/SpansEdge.java
+++ engine/src/main/java/nl/inl/blacklab/search/lucene/SpansEdge.java
@@ -7,24 +7,24 @@ import java.io.IOException;
 import org.apache.lucene.search.spans.FilterSpans;
 
 /**
- * Returns either the left edge or right edge of the specified query.
+ * Returns either the leading or trailing (left or right) edge of the specified query.
  * <p>
  * Note that the results of this query are zero-length spans.
  */
 class SpansEdge extends BLFilterSpans<BLSpans> {
 
-    /** if true, return the right edges; if false, the left */
-    private final boolean rightEdge;
+    /** if true, return the trailing (e.g right in LTR languages) edge; if false, the leading one */
+    private final boolean trailingEdge;
 
     /**
      * Constructs a SpansEdge.
      *
      * @param clause the clause to get an edge from
-     * @param rightEdge whether or not to get the right edge
+     * @param trailingEdge if true, return trailing edge, otherwise leading one
      */
-    public SpansEdge(BLSpans clause, boolean rightEdge) {
+    public SpansEdge(BLSpans clause, boolean trailingEdge) {
         super(clause);
-        this.rightEdge = rightEdge;
+        this.trailingEdge = trailingEdge;
     }
 
     @Override
@@ -34,7 +34,7 @@ class SpansEdge extends BLFilterSpans<BLSpans> {
 
     @Override
     public int startPosition() {
-        return atFirstInCurrentDoc ? -1 : (rightEdge ? in.endPosition() : startPos);
+        return atFirstInCurrentDoc ? -1 : (trailingEdge ? in.endPosition() : startPos);
     }
 
     @Override
@@ -45,12 +45,12 @@ class SpansEdge extends BLFilterSpans<BLSpans> {
     @Override
     public int nextStartPosition() throws IOException {
         super.nextStartPosition();
-        return rightEdge ? in.endPosition() : startPos;
+        return trailingEdge ? in.endPosition() : startPos;
     }
 
     @Override
     public int advanceStartPosition(int target) throws IOException {
-        if (rightEdge) {
+        if (trailingEdge) {
             // We can't skip because the spans we produce are not sorted by start.
             // Use the naive implementation.
             BLSpans.naiveAdvanceStartPosition(this, target);
@@ -61,7 +61,7 @@ class SpansEdge extends BLFilterSpans<BLSpans> {
 
     @Override
     public String toString() {
-        return "EDGE(" + in + ", " + (rightEdge ? "RIGHT" : "LEFT") + ")";
+        return "EDGE(" + in + ", " + (trailingEdge ? "TRAILING" : "LEADING") + ")";
     }
 
 }
